bb8a5e67c714b6dd8fb2357504b40c5c42bdaebe
Dinara Idrissova <Dinara.Idrissova@list.ru>
2024-01-03 12:23:23 -0500

    plain JSX

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,22 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <div>hello</div>
+      <form action="" className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="form-row" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" /> Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
     </>
   );
 }
